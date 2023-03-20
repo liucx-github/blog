0adbaf168c613dd112e773de5bd89c1d4d9cbc69
liucx <980424881@qq.com>
2023-03-20 14:20:17 +0800

    fix: test

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ http.createServer(async (req, res) => {
       const data = await resolvePost(req);
       const projectDir = path.resolve(`./${data.repository.name}`)
      deleteFolderRecursive(projectDir)
-     console.log(`git clone https://github.com/liucx-github/${data.repository.name}.git`)
+     console.log(`git clone https://github.com/liucx-github/${data.repository.name}.git ${projectDir}`)
       // 拉取仓库最新代码
       execSync(`git clone https://github.com/liucx-github/${data.repository.name}.git ${projectDir}`,{
         stdio:'inherit',
